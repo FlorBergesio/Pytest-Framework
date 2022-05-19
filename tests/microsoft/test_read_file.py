f6f1ebf117989f35ecbc2b415f9ec0d05d951d45
FlorBergesioU <florencia.bergesio@uala.com.ar>
2022-05-19 10:45:52 -0300

    Added type check in admin_command

--- tests/microsoft/test_read_file.py
+++ tests/microsoft/test_read_file.py
@@ -1,3 +1,5 @@
+import pytest
+
 class AdminCommandTests:
 
     def command(self):
@@ -12,12 +14,19 @@ class AdminCommandTests:
         expected = ["sudo"] + self.command()
         assert result == expected
 
+    def test_non_list_commands(self):
+        with pytest.raises(TypeError) as error:
+            admin_command("some command", sudo=True)
+        assert error.value.args[0] == "was expecting command to be a list, but got a <class 'str'>"
+
 
 def admin_command(command, sudo=True):
     """
     Prefix a command with `sudo` unless it is explicitly not needed. Expects
     `command` to be a list.
     """
+    if not isinstance(command, list):
+        raise TypeError(f"was expecting command to be a list, but got a {type(command)}")
     if sudo:
         return ["sudo"] + command
     return command
