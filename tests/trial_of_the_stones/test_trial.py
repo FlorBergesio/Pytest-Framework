c0cf19843295bab6e0a9f678e56d808fb11c530a
FlorBergesioU <florencia.bergesio@uala.com.ar>
2022-05-31 10:39:46 -0300

    Added test to check seleniun wemdriver functionality and the resolution of the Riddle of the Stone

---
+++ tests/trial_of_the_stones/test_trial.py
@@ -0,0 +1,32 @@
+from operator import truediv
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+def test_selenium_automated_input_value():
+    browser = webdriver.Chrome()
+    browser.get("https://techstepacademy.com/training-ground")
+    input1Path = "input[id='ipt1']"
+    input1Element = browser.find_element(By.CSS_SELECTOR, input1Path)
+    input1Element.send_keys("Automated input content")
+    time.sleep(1)
+    assert input1Element.get_attribute("value") == "Automated input content"
+
+def test_riddle_of_stone():
+    browser = webdriver.Chrome()
+    browser.get("https://techstepacademy.com/trial-of-the-stones")
+    inputStonePath = "input[id='r1Input']"
+    inputStoneElement = browser.find_element(By.CSS_SELECTOR, inputStonePath)
+    inputStoneElement.send_keys("rock")
+    time.sleep(1)
+    buttonStonePath = "button[id='r1Btn']"
+    buttonStoneElement = browser.find_element(By.CSS_SELECTOR, buttonStonePath)
+    buttonStoneElement.click()
+    time.sleep(1)
+    passwordContainerStonePath = "div[id='passwordBanner']"
+    passwordContainerStoneElement = browser.find_element(By.CSS_SELECTOR, passwordContainerStonePath)
+    """ passwordStonePath = "//div[@id='passwordBanner']/h4"
+    passwordStoneElement = browser.find_element(By.XPATH, passwordStonePath) """
+    time.sleep(1)
+
+    assert passwordContainerStoneElement.get_attribute("style") == "display: block;"
