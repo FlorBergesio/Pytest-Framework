ad783c112c1737335271a2360698adb36962752f
FlorBergesioU <florencia.bergesio@uala.com.ar>
2022-06-01 10:29:18 -0300

    Finished attempt to pass the Trial of the Stones coding challenge - successful

--- tests/trial_of_the_stones/test_trial.py
+++ tests/trial_of_the_stones/test_trial.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from operator import attrgetter
 import time
 
 def test_selenium_automated_input_value(chrome_browser):
@@ -22,8 +23,47 @@ def test_riddle_of_stone(chrome_browser):
     time.sleep(1)
     passwordContainerStonePath = "div[id='passwordBanner']"
     passwordContainerStoneElement = chrome_browser.find_element(By.CSS_SELECTOR, passwordContainerStonePath)
-    """ passwordStonePath = "//div[@id='passwordBanner']/h4"
-    passwordStoneElement = chrome_browser.find_element(By.XPATH, passwordStonePath) """
+    passwordStonePath = "//div[@id='passwordBanner']/h4"
+    passwordStoneElement = chrome_browser.find_element(By.XPATH, passwordStonePath)
     time.sleep(1)
-
-    assert passwordContainerStoneElement.get_attribute("style") == "display: block;"
+    if passwordContainerStoneElement.get_attribute("style") == "display: block;":
+        inputSecretPath = "input[id='r2Input']"
+        inputSecretElement = chrome_browser.find_element(By.CSS_SELECTOR, inputSecretPath)
+        inputSecretElement.send_keys(passwordStoneElement.get_attribute("innerHTML"))
+        time.sleep(1)
+        buttonSecretPath = "button[id='r2Butn']"
+        buttonSecretElement = chrome_browser.find_element(By.CSS_SELECTOR, buttonSecretPath)
+        buttonSecretElement.click()
+        time.sleep(1)
+        successContainerSecretPath = "div[id='successBanner1']"
+        successContainerSecretElement = chrome_browser.find_element(By.CSS_SELECTOR, successContainerSecretPath)
+        if successContainerSecretElement.get_attribute("style") == "display: block;":
+            wealthListPath = "//label/../p"
+            wealthList = chrome_browser.find_elements(By.XPATH, wealthListPath)
+            maxWealth = 0
+            for wealth in wealthList:
+                if int(wealth.get_attribute("innerHTML")) > maxWealth:
+                    maxWealth = int(wealth.get_attribute("innerHTML"))
+            richestMerchantPath = "//label/../p[contains(text(),'" + str(maxWealth) + "')]/../span/b"
+            richestMerchantElement = chrome_browser.find_element(By.XPATH, richestMerchantPath)
+            inputMerchantPath = "input[id='r3Input']"
+            inputMerchantElement = chrome_browser.find_element(By.CSS_SELECTOR, inputMerchantPath)
+            inputMerchantElement.send_keys(richestMerchantElement.get_attribute("innerHTML"))
+            time.sleep(1)
+            buttonMerchantPath = "button[id='r3Butn']"
+            buttonMerchantElement = chrome_browser.find_element(By.CSS_SELECTOR, buttonMerchantPath)
+            buttonMerchantElement.click()
+            time.sleep(1)
+            successContainerMerchantPath = "div[id='successBanner1']"
+            successContainerMerchantElement = chrome_browser.find_element(By.CSS_SELECTOR, successContainerMerchantPath)
+            if successContainerMerchantElement.get_attribute("style") == "display: block;":
+                buttonFinalPath = "button[id='checkButn']"
+                buttonFinalElement = chrome_browser.find_element(By.CSS_SELECTOR, buttonFinalPath)
+                buttonFinalElement.click()
+                time.sleep(1)
+                successContainerFinalPath = "div[id='trialCompleteBanner']"
+                successContainerFinalElement = chrome_browser.find_element(By.CSS_SELECTOR, successContainerFinalPath)
+                if successContainerFinalElement.get_attribute("style") == "display: block;":
+                    successTextPath = "//div[@id='trialCompleteBanner']/h4"
+                    successTextElement = chrome_browser.find_element(By.XPATH, successTextPath)
+                    assert successTextElement.get_attribute("innerHTML") == "Trial Complete"
