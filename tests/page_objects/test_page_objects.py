2ed6fd816e095c21441be03b5316115fd3cfab4b
FlorBergesioU <florencia.bergesio@uala.com.ar>
2022-06-11 14:17:08 -0300

    Added test to check text of button and made test of button click more useful

--- tests/page_objects/test_page_objects.py
+++ tests/page_objects/test_page_objects.py
@@ -1,6 +1,8 @@
 from .page_object_model import TrainingGroundPage
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support.expected_conditions import visibility_of, alert_is_present
+from selenium.webdriver.support.expected_conditions import alert_is_present
+from selenium.webdriver.common.alert import Alert
+
 from pytest import mark
 
 @mark.skip(reason="Original code has been commented out. New code not yet created")
@@ -18,4 +20,12 @@ def test_page_object_base_element_button_click(chrome_browser):
     training_ground_page = TrainingGroundPage(driver=chrome_browser)
     training_ground_page.go()
     training_ground_page.button1.click()
+    WebDriverWait(chrome_browser, 10).until(alert_is_present())
+    alertElement = Alert(chrome_browser)
+    assert alertElement.text == "You clickedButton1."
     assert True
+
+def test_page_object_base_element_button_text(chrome_browser):
+    training_ground_page = TrainingGroundPage(driver=chrome_browser)
+    training_ground_page.go()
+    assert training_ground_page.button1.text == 'Button1'
